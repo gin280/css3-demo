122ef2cb49c36e3c05d923b3f22258fe5334b6ba
gin280 <gin280@163.com>
2016-06-07 16:44:58 +0800

    'father1'

---
+++ app/fatherday/dom.js
@@ -0,0 +1,1 @@
+!function(a,b){window.devicePixelRatio&&devicePixelRatio>=2&&$("body").addClass("hairlines");var c=a.documentElement,d="orientationchange"in window?"orientationchange":"resize",e=function(){var a=c.clientWidth;a&&(c.style.fontSize=10*(a/320)+"px")};a.addEventListener&&(b.addEventListener(d,e,!1),a.addEventListener("DOMContentLoaded",e,!1))}(document,window);
